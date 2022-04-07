968f5f7747c71f72425460b740f2b8c6517c3813
Tarik Eshaq <teshaq@mozilla.com>
2022-04-07 11:34:13 -0700

    Adds rondpoint fixture

---
+++ toolkit/components/uniffi-bindgen-gecko-js/fixtures/tests/test_rondpoint.js
@@ -0,0 +1,6 @@
+/* Any copyright is dedicated to the Public Domain.
+   http://creativecommons.org/publicdomain/zero/1.0/ */
+
+const Rondpoint = ChromeUtils.import("resource://gre/modules/components-utils/Rondpoint.jsm");
+
+Assert.ok(true);
