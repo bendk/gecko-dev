71cbff12658dd3c2f4db8462998a4007576f402c
Tarik Eshaq <teshaq@mozilla.com>
2022-04-07 14:59:15 -0700

    adds test for sequences of enums

--- toolkit/components/uniffi-bindgen-gecko-js/fixtures/tests/test_rondpoint.js
+++ toolkit/components/uniffi-bindgen-gecko-js/fixtures/tests/test_rondpoint.js
@@ -3,12 +3,13 @@
 
 const Rondpoint = ChromeUtils.import("resource://gre/modules/components-utils/Rondpoint.jsm");
 
-const { Enumeration, EnumerationAvecDonnees, copieEnumeration, copieCarte } = Rondpoint;
+const { Enumeration, EnumerationAvecDonnees, copieEnumeration, copieEnumerations, copieCarte } = Rondpoint;
 
 add_task(async function() {
    const un = Enumeration.UN;
-   const duex = Enumeration.DUEX;
+   const deux = Enumeration.DEUX;
    const enumeration = await copieEnumeration(un);
+   const enumerations = await copieEnumerations([un, deux]);
    const zero = new EnumerationAvecDonnees.Zero();
    const obj = {
       "zero": zero
@@ -18,6 +19,7 @@ add_task(async function() {
    Assert.equal(enumeration, un);
    Assert.ok(enumeration === Enumeration.UN);
    Assert.deepEqual(obj, ret);
+   Assert.deepEqual(enumerations, [un, deux]);
    Assert.ok(ret.zero instanceof EnumerationAvecDonnees.Zero)
    Assert.ok(ret.zero instanceof EnumerationAvecDonnees)
 })
